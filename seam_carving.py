6db5c696e50e9e67b3d80120470a1be25260e6d5
Christopher Acciai <40211456+chrisacciai@users.noreply.github.com>
2019-10-30 03:29:39 -0400

    Update seam_carving.py

--- seam_carving.py
+++ seam_carving.py
@@ -1,27 +1,8 @@
-# CS4102 Fall 2019 -- Homework 5
-#################################
-# Collaboration Policy: You are encouraged to collaborate with up to 4 other
-# students, but all work submitted must be your own independently written
-# solution. List the computing ids of all of your collaborators in the comment
-# at the top of your java or python file. Do not seek published or online
-# solutions for any assignments. If you use any published or online resources
-# (which may not include solutions) when completing this assignment, be sure to
-# cite them. Do not submit a solution that you are unable to explain orally to a
-# member of the course staff.
-#################################
-# Your Computing ID: cga4yc
-# Collaborators: arg5nq
-# Sources: Introduction to Algorithms, Cormen
-#################################
 class SeamCarving:
     seamCoordinates = []
     def __init__(self):
         return
-
-    # This method is the one you should implement.  It will be called to perform
-    # the seam carving.  You may create any additional data structures as fields
-    # in this class or write any additional methods you need.
-    # 
+    
     # @return the seam's weight
     def run(self, image):
 
@@ -86,13 +67,8 @@ class SeamCarving:
                 
         return min(mem[num_rows - 1])
 
-    # Get the seam, in order from top to bottom, where the top-left corner of the
-    # image is denoted (0,0).
-    # 
-    # Since the y-coordinate (row) is determined by the order, only return the x-coordinate
-    # 
     # @return the ordered list of x-coordinates (column number) of each pixel in the seam
-    #         as an array
+    # as an array
     def getSeam(self):
         return self.seamCoordinates
 
